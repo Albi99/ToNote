efef52821e3a51b7ef03f5dc384b2a66e6a2a1d5
Albi <57755917+Albi99@users.noreply.github.com>
2025-01-23 17:05:44 +0100

    Implementazione più rigida dell'architettura MVC

--- ToNote/app/controllers.py
+++ ToNote/app/controllers.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from app.models import db, Note, Todo
+from app import models
 
 main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
 def index():
-    notes = Note.query.all()
-    todos = Todo.query.all()
+    notes = models.Note.get_all()
+    todos = models.Todo.get_all()
     return render_template('index.html', notes=notes, todos=todos)
 
 
@@ -16,27 +16,22 @@ def index():
 @main_blueprint.route('/add_note', methods=['POST'])
 def add_note():
     content = request.form.get('content')
-    if content:
-        new_note = Note(content=content)
-        db.session.add(new_note)
-        db.session.commit()
+    models.Note.add(content)
     return redirect(url_for('main.index'))
 
 @main_blueprint.route('/delete_note/<int:note_id>', methods=['POST'])
 def delete_note(note_id):
-    note = Note.query.get_or_404(note_id)
-    db.session.delete(note)
-    db.session.commit()
+    models.Note.delete(note_id)
     return redirect(url_for('main.index'))
 
 @main_blueprint.route('/edit_note/<int:note_id>', methods=['GET', 'POST'])
 def edit_note(note_id):
-    note = Note.query.get_or_404(note_id)
+    note = models.Note.get(note_id)
     if request.method == 'POST':
+        print('Inside POST')
         new_content = request.form.get('content')
         if new_content:
-            note.content = new_content
-            db.session.commit()
+            models.Note.edit(note, new_content)
             return redirect(url_for('main.index'))
     return render_template('edit_note.html', note=note)
 
@@ -48,22 +43,15 @@ def edit_note(note_id):
 @main_blueprint.route('/add_todo', methods=['POST'])
 def add_todo():
     task = request.form.get('task')
-    if task:
-        new_task = Todo(task=task)
-        db.session.add(new_task)
-        db.session.commit()
+    models.Todo.add(task)
     return redirect(url_for('main.index'))
 
 @main_blueprint.route('/toggle_todo/<int:todo_id>', methods=['POST'])
 def toggle_todo(todo_id):
-    todo =  Todo.query.get_or_404(todo_id)
-    todo.is_done = not todo.is_done
-    db.session.commit()
+    models.Todo.toggle(todo_id)
     return redirect(url_for('main.index'))
 
 @main_blueprint.route('/delete_todo/<int:todo_id>', methods=['POST'])
 def delete_todo(todo_id):
-    todo = Todo.query.get_or_404(todo_id)
-    db.session.delete(todo)
-    db.session.commit()
+    models.Todo.detete(todo_id)
     return redirect(url_for('main.index'))
--- ToNote/app/models.py
+++ ToNote/app/models.py
@@ -2,12 +2,69 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
+# NOTE
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(512), nullable=False)
+
+    @staticmethod
+    def add(content):
+        if content:
+            new_note = Note(content=content)
+            db.session.add(new_note)
+            db.session.commit()
+
+    @staticmethod
+    def edit(note, new_content):
+        note.content = new_content
+        db.session.commit()
+
+    @staticmethod
+    def delete(id):
+        note = Note.query.get_or_404(id)
+        db.session.delete(note)
+        db.session.commit()
+
+    @staticmethod
+    def get(id):
+        return Note.query.get_or_404(id)
+
+    @staticmethod
+    def get_all():
+        return Note.query.all()
+
+
+
+# TODO
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(128), nullable=False)
     is_done = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
+
+    @staticmethod
+    def add(task):
+        if task:
+            new_task = Todo(task=task)
+            db.session.add(new_task)
+            db.session.commit()
+
+    @staticmethod
+    def toggle(id):
+        todo =  Todo.query.get_or_404(id)
+        todo.is_done = not todo.is_done
+        db.session.commit()
+    
+    @staticmethod
+    def detete(id):
+        todo = Todo.query.get_or_404(id)
+        db.session.delete(todo)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Todo.query.all()
